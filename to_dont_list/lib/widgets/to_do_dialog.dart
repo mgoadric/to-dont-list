1bae4cddf7ca3f328faa4854dd961301ae73df62
nobenx <noben.x@gmail.com>
2026-09-14 21:31:32 -0500

    Ditto

--- to_dont_list/lib/widgets/to_do_dialog.dart
+++ to_dont_list/lib/widgets/to_do_dialog.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 typedef ToDoListAddedCallback = Function(
-    String value, TextEditingController textController);
+    String value, TextEditingController textController, double? time);
 
 class ToDoDialog extends StatefulWidget {
   const ToDoDialog({
@@ -24,20 +25,40 @@ class _ToDoDialogState extends State<ToDoDialog> {
       textStyle: const TextStyle(fontSize: 20), backgroundColor: Colors.red);
 
   String valueText = "";
+  double? valueTime = 0.0;
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text('Item To Add'),
-      content: TextField(
-        onChanged: (value) {
-          setState(() {
-            valueText = value;
-          });
-        },
-        controller: _inputController,
-        decoration: const InputDecoration(hintText: "type something here"),
-      ),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextField(
+            onChanged: (value) {
+              setState(() {
+                valueText = value;
+              });
+            },
+            controller: _inputController,
+            decoration: const InputDecoration(hintText: "type something here"),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            inputFormatters: <TextInputFormatter> [
+              FilteringTextInputFormatter.digitsOnly,
+            ],
+            onChanged:(value){
+              setState(() {
+                valueTime = double.tryParse(value) ?? 0.0;
+              });
+            },
+            decoration: const InputDecoration(
+              labelText: "Seconds",
+            )
+          )
+        ],
+      ), 
       actions: <Widget>[
         ElevatedButton(
           key: const Key("CancelButton"),
@@ -60,7 +81,7 @@ class _ToDoDialogState extends State<ToDoDialog> {
               onPressed: value.text.isNotEmpty
                   ? () {
                       setState(() {
-                        widget.onListAdded(valueText, _inputController);
+                        widget.onListAdded(valueText, _inputController, valueTime);
                         Navigator.pop(context);
                       });
                     }
